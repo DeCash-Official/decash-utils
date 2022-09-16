47acc9b839cd777507f33bc72b41005b542064cc
Roberto Pettinau <roberto.pettinau99@gmail.com>
2022-09-16 22:58:36 +0200

    Add test case for initializeBinaryContract

--- tests/nodejs/utility.test.ts
+++ tests/nodejs/utility.test.ts
@@ -8,7 +8,9 @@ import {
   uInt8ArrayToBase64,
   uInt8ArrayToVarInt,
   varIntToUInt8Array,
+  initializeBinaryContract,
 } from '../../lib';
+import { uInt8ArrayEquals, uInt8ArraysConcat } from '../../lib/Utils';
 
 describe('Utility functions', () => {
   it('algorandAddress -> uInt8Array -> algorandAddress', () => {
@@ -37,4 +39,47 @@ describe('Utility functions', () => {
       'BSACAAEmASCaFzX+fxiZrczt6PwM6d2y5lB3ztmmAhGgcmaaoVDvDChIgYCAgICAgICAAEgxIDIDEkQtFyMSQACHLRciEkAAeAAiNQA0ADIEDEEAfjQAOBCBBhJAAA8iQAAJNAAjCDUAQv/hI0M0ADgYgeCq7qGAgICAABJAAAQiQv/cNAA4GzEWDUAABCJC/840ADEWwhoiI1I0AUAAG4ABThJAAAQiQv+1NAA0AMIaIiNSgAF2EkL/pYABWUL/4ogAEDUBQv+BLjEXKAQ1AUL/dwAiNQA0ADIEDEEAFDQAOAAoEkAACTQAIwg1AEL/5iOJIok=';
     expect(uInt8ArrayToBase64(base64ToUInt8Array(b64))).to.be.equal(b64);
   });
+
+  it('Initialize binary contract', () => {
+    const templateBytecodeBase64 = btoa('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!"£$%&/()=?^[]{}@#')
+    const templateBytecodeUinta = base64ToUInt8Array(templateBytecodeBase64)
+    const template: {
+      templateBytecodeBase64: string;
+      interpolated: {
+        [key: string]: {
+          type: 'address' | 'varint';
+          fromByte: number;
+          lengthBytes: number;
+        };
+      };
+    } = {
+      templateBytecodeBase64,
+      interpolated: {
+        test_a: {
+          type: 'address',
+          fromByte: 8,
+          lengthBytes: 32,
+        },
+        test_b: {
+          type: 'varint',
+          fromByte: 64,
+          lengthBytes: 9,
+        },
+      },
+    };
+    const result = initializeBinaryContract(template, {
+      test_a: 'PUQHXUYYKNFSMQ4PTNT647WOKZ2622OZDBDXNIPJJIZJR6YUI3WQICVFHA',
+      test_b: 245
+    });
+
+    expect(uInt8ArrayEquals(templateBytecodeUinta, result)).to.be.equal(false);
+    expect(uInt8ArrayEquals(templateBytecodeUinta, result)).to.be.equal(false);
+    expect(uInt8ArrayEquals(result, uInt8ArraysConcat(
+      base64ToUInt8Array(btoa('abcdefgh')),
+      algorandAddressToUInt8Array('PUQHXUYYKNFSMQ4PTNT647WOKZ2622OZDBDXNIPJJIZJR6YUI3WQICVFHA'),
+      base64ToUInt8Array(btoa('OPQRSTUVWXYZ1234567890!"')),
+      varIntToUInt8Array(245, { padToBytes: 9 }),
+      base64ToUInt8Array(btoa('^[]{}@#'))
+    ))).to.be.equal(true);
+  })
 });
